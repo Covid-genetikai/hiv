4383762136181f1a093b1b73a319ac954fd7f16e
Valdas Rapsevicius <vrapsevicius@gmail.com>
2022-01-04 19:28:50 +0200

    Updated script

--- 1-build-dataset.py
+++ 1-build-dataset.py
@@ -1,113 +1,97 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[60]:
-
-
 from Bio import GenBank
 from pathlib import Path
 import pandas as pd
-
-
-# In[22]:
-
+import re, csv
 
 data_path = "/hiv/data"
 data_path = "/home/mantydze/hiv/hiv"
+data_path = "data"
+
+GENES = ['pol', 'env', 'gag', 'vpr', 'vif', 'tat', 'rev', 'vpu', 'nef']
 
 
-# In[58]:
+def read_qualifiers(feature, *args):
+    resp = [ None for n in args ]
+    shit_names = [ f"/{n}=" for n in args ]
+    for qualifier in feature.qualifiers:
+        if qualifier.key in shit_names:
+            i = shit_names.index(qualifier.key)
+            resp[i] = qualifier.value.replace('"', '').replace("'", "")
+    return tuple(resp)
+
+
+def parse_record(record):
+
+    obj = None
+
+    if 'HIV' in record.source:
+
+        obj = {
+            "accession": record.accession[0],
+            'length': len(record.sequence),
+            "sequence": record.sequence
+        }
+
+        for gene in GENES:
+            obj[gene + "_pro"] = None
+            obj[gene + "_loc"] = None
+
+        for feature in record.features:
+
+            if feature.key == "source":
+                obj["country"] = read_qualifiers(feature, 'country')[0]
+                continue
+
+            if feature.key == "gene":
+                location = feature.location
+                gene = read_qualifiers(feature, 'gene')[0]
+                if gene is not None and location is not None:
+                    gene = gene.lower()
+                    if gene in GENES:
+                        obj[gene + "_loc"] = location
+                continue
+
+            if feature.key == "CDS":
+
+                resp = read_qualifiers(feature, 'gene', 'translation')
+                gene, protein = resp[0], resp[1]
+
+                if protein is not None and gene is not None and gene in GENES:
+                    obj[gene + "_pro"] = protein
+                continue
+
+    return obj
 
 
 def parse_file(filename):
     data = []
     with open(filename, "r") as handle:
-        for record in GenBank.parse(handle):
+        records = GenBank.parse(handle)
+        while True:
+            try:
+                obj = parse_record(record=records.__next__())
+                if obj is not None:
+                    data.append(obj)
 
-            # Mums idomus tik tiek kur yra 'HIV' SOURCE eilutej
-            ## SOURCE      Human immunodeficiency virus 1 (HIV-1)
-            if 'HIV' in record.source:
+            except StopIteration as e:
+                break
+            except Exception as e:
+                print(e, "Ignoring..")
+                pass
 
-                obj = {
-                    "accession": record.accession[0], 
-                    'length': len(record.sequence),
-                    "sequence": record.sequence       
-                    
-                }
-                
-                #[Feature(key='source', location='1..1686'), Feature(key='gene', location='<1..>1686'), Feature(key='CDS', location='<1..>1686')]
-                for feature in record.features:
-#                     print(feature.key)
-                    if feature.key == "source":
-                        # [Qualifier(key='/organism=', value='"Human immunodeficiency virus 1"'), Qualifier(key='/proviral', value=''), Qualifier(key='/mol_type=', value='"genomic DNA"'), Qualifier(key='/db_xref=', value='"taxon:11676"'), Qualifier(key='/country=', value='"Spain"')]
-#                         for qualifier in qualifiers:
-                                
-                        for qualifier in feature.qualifiers:
-                            if qualifier.key == "/country=":
-                                obj["country"] = qualifier.value.replace('"','').replace("'", "")
-                    
-                    
-                    if feature.key == "gene":
-                        pass
-                    
-                    if feature.key == "CDS":
-                        gene = None
-                        protein = None
-                        
-                        for qualifier in feature.qualifiers:
-                            if qualifier.key == "/gene=":
-                                gene = qualifier.value.replace('"','').replace("'", "").lower()
-                            
-                            if qualifier.key == "/translation=":
-                                protein = qualifier.value.replace('"','').replace("'", "")
-                        
-                       # genu pavadinimuose yra nemazai siuksliu, camel case ir t.t.
-                    
-                       # ['pol', 'env', 'gag',
-                       #'vpr', 'vif', 'tat', 'rev', 'vpu', 'nef', 'gag-pol', 'rev1', 'gp120',
-                       #'reverse transcriptase', 'HIV-1 protease', 'RT', 'protease', 'PR',
-                       #'p24', 'envelope', 'gp160', 'tat/rev', 'vpu*', 'rt', 'ORF',
-                       #'v-1 PROTEASE', 'Gag', 'Pol', 'Vif', 'Vpr', 'Tat', 'Rev', 'Vpu', 'Env',
-                       #'Nef', 'pro', 'as', 'v-1 reverse transcriptase', 'v-1 protease', 'Pro',
-                       #'V-1 protease', 'V-1 reverse transcriptase', 'polyprotein', 'rnv', 'gg',
-                       #'env V3', 'gp41', 'GP160', 'GP120', 'GP41', 'env gene', 'RAK alpha',
-                       #'V3', 'c2v3', 'vpx']
-                        
-                        if protein and gene and gene in ['pol', 'env', 'gag', 'vpr', 'vif', 'tat', 'rev', 'vpu', 'nef']:
-                            obj[gene] = protein                           
-                                
-                    
-                        
-#                     print(feature)
-#                     print(dir(feature))
-                    
-#                     print("---")
-                    
-                
-                data.append(obj)
-                
     return data
-                
 
 
-# In[61]:
-
-
-data = []
 for filename in Path(data_path).glob("*.seq"):
+
     print(f"Parsing {filename}")
-    data.extend(parse_file(filename))
+    data = parse_file(filename)
+    df = pd.DataFrame(data)
 
-
-# In[62]:
-
-
-df = pd.DataFrame(data)
-df
-
-
-# In[69]:
-
-
-df.notnull().sum()
+    filename = re.sub("\\.seq$",".csv", str(filename))
+    df.to_csv(filename, quoting=csv.QUOTE_NONNUMERIC)
+    print(f"Wrote {filename} len {len(df)}")
 
